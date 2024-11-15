77cfb7456721925081392f570cd15e78340182af
asad406 <asadul76@gmail.com>
2024-11-15 08:22:48 +0600

    comment updated

--- sync-file.js
+++ sync-file.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-//reading a file text
+//reading a file text synchronously
 const readText = fs.readFileSync('./texts/read.txt','utf-8');
 console.log(readText);
 
-//writing a file text
+//writing a file text synchronously
 const writingText = fs.writeFileSync('./texts/wirte.txt', readText+ 'this is my writing text')
 console.log(writingText);
