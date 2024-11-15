af063977aa57b9d0510ab33626bfa85885abc504
asad406 <asadul76@gmail.com>
2024-11-15 07:20:18 +0600

    file updated

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 //built in module
+
+const path = require("path");
+console.log(path.dirname("D:/Mission-2.Node/node-built-in-module/index.js"));
+//Here must use /(forward slash)
+console.log(path.parse("D:/Mission-2.Node/node-built-in-module/index.js"));
+console.log(path.join("D:/Mission-2.Node/node-built-in-module/","index.js"));
